b1154e30266582e17c197a8e656393902dc7a644
Thomas Volk (MacBook) <info@thomasvolk.de>
2021-03-24 20:58:16 +0100

    rules test

--- src/CellularAutomat.spec.ts
+++ src/CellularAutomat.spec.ts
@@ -81,3 +81,49 @@ describe('cycle function', () => {
     });
 });
 
+describe('ConwayAlgorithm', () => {
+    it('should normalizeToOneOrZero', () => {
+        expect(
+            ConwayAlgorithm.normalizeToOneOrZero([5, 0.7, 0, 1, -1, 0]).join(",")
+        ).to.equal("1,1,0,1,0,0")
+    });
+    it('should implement the rules correctly', () => {
+        const testData = [
+            {
+                'cell': 1,
+                'neighbours': [1, 1, 0, 0, 0, 0],
+                'expected': 1
+            },
+            {
+                'cell': 1,
+                'neighbours': [1, 1, 0, 0, 0, 0, 0, 0, 0],
+                'expected': 1
+            },
+            {
+                'cell': 0,
+                'neighbours': [1, 1, 0, 0, 0, 0, 0, 0, 0],
+                'expected': 0
+            },
+            {
+                'cell': 0,
+                'neighbours': [1, 1, 1, 0, 0, 0, 0, 0, 0],
+                'expected': 1
+            },
+            {
+                'cell': 1,
+                'neighbours': [1, 1, 1, 1, 0, 0, 0, 0, 0],
+                'expected': 0
+            },
+            {
+                'cell': 1,
+                'neighbours': [1, 0, 0, 0, 0, 0, 0, 0, 0],
+                'expected': 0
+            }
+        ]
+        testData.forEach(testSet => {
+            expect(
+                ConwayAlgorithm.calculateCellValue(testSet.cell, testSet.neighbours)
+            ).to.equal(testSet.expected);
+        });
+    });
+});
